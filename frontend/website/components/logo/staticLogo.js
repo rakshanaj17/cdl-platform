2d9419fa23a8b4e1ac524fce6a10a5f293e68116
Rakshana <rj17@illinois.edu>
2024-02-23 11:58:10 -0600

    svg file base added

---
+++ frontend/website/components/logo/staticLogo.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const StaticLogo = () => {
+    return (
+        <svg width="100" height="100" viewBox="0 0 100 100">
+            <circle cx="50" cy="50" r="40" fill="blue" />
+        </svg>
+    );
+};
+
+export default StaticLogo;
